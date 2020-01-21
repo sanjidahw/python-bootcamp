91f139f11dd45836f208306ff88f95ef2394e8da
sanjidahw <sanjidahw@gmail.com>
2020-01-21 02:00:23 -0500

    Assignment 2 Completed

--- Week 2/Assignment2.py
+++ Week 2/Assignment2.py
@@ -15,7 +15,7 @@ for value in students.values():
 	#print(value)
 	pass
 
-#2	Add a key to the dictionary
+#2 Add a key to the dictionary
 students["Num of absences"] = [1298, 5, 3, 1, 0, 2]
 
 #3 Create a function to find the average GPA of all the students. 
@@ -36,7 +36,7 @@ def highest(absences,N):
 absences = students["Num of absences"]
 N = len(absences)
 ans = highest(absences,N)
-print(ans)
+#print(ans)
 
 #5 Reset all the student’s number of absences to 0. 
 students.pop("Num of absences")
@@ -49,10 +49,10 @@ for value in students.values():
 
 #6  Say that a new student has joined this small school. 
 #Append this student to the end of every list. 
-#list_of_names.append("Fernando")
-#list_of_majors.append("EE")
-#students["GPA"].append(3.2)
-#students["Num of absences"].append(0)
-#for value in students.values():
+list_of_names.append("Fernando")
+list_of_majors.append("EE")
+students["GPA"].append(3.2)
+students["Num of absences"].append(0)
+for value in students.values():
 	#print(value)
-#	pass
+	pass
